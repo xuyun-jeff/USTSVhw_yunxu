31865050e7cb003f8cf098dff5beffc4e989c5ac
xuyun-jeff <xuyun7182862@gmail.com>
2020-04-29 00:30:18 -0700

    updated(04/29/2020)

--- SQL_YunXu.sql
+++ SQL_YunXu.sql
@@ -215,21 +215,23 @@ SELECT SUM(quantityOrdered*priceEach) as OrderValue FROM orderdetails
 JOIN orders ON orders.orderNumber = orderdetails.orderNumber
 WHERE year(shippedDate) = 2004
 AND monthname(shippedDate) = 'August';
--- (not solved) 8. Compute the total value ordered, total amount paid, and their difference for each customer for orders placed in 2004 and payments received in 2004 (Hint; Create views for the total paid and total ordered).
-SELECT orders.orderNumber, SUM(quantityOrdered*priceEach), SUM(amount), (DISTINCT orders.customerNumber) FROM orderdetails
-JOIN orders ON orderdetails.orderNumber = orders.orderNumber
-JOIN payments ON orders.customerNumber = payments.customerNumber
-GROUP BY orders.orderNumber
+-- 8. Compute the total value ordered, total amount paid, and their difference for each customer for orders placed in 2004 and payments received in 2004 (Hint; Create views for the total paid and total ordered).
+CREATE VIEW total_order AS
+SELECT orders.customerNumber, SUM(orderdetails.quantityOrdered*orderdetails.priceEach) as totalordervalue FROM orders
+JOIN orderdetails ON orderdetails.orderNumber = orders.orderNumber
+WHERE YEAR(orders.orderDate)=2004
+GROUP BY orders.customerNumber;
 
-SELECT customerNumber, SUM(amount) FROM payments
-GROUP BY customerNumber
+CREATE VIEW total_paid AS
+SELECT orders.customerNumber, SUM(payments.amount) as totalpaidvalue FROM payments
+JOIN orders ON orders.customerNumber = payments.customerNumber
+WHERE YEAR(orders.orderDate)=2004
+GROUP BY orders.customerNumber;
 
-SELECT orderdetails.orderNumber, SUM(quantityOrdered*priceEach), payments.customerNumber FROM orderdetails
-JOIN orders ON orders.orderNumber = orderdetails.orderNumber
-JOIN payments ON payments.customerNumber = orders.customerNumber
-GROUP BY checkNumber
-
-payments.customerNumber, SUM(quantityOrdered*priceEach) as totalOrdered, SUM(amount) as totalPay 
+SELECT total_order.customerNumber, total_order.totalordervalue, total_paid.totalpaidvalue, (totalpaidvalue-totalordervalue) as difference FROM total_order
+JOIN total_paid ON total_order.customerNumber = total_paid.customerNumber
+ORDER BY customerNumber
+ 
 -- 9. List the employees who report to those employees who report to Diane Murphy. Use the CONCAT function to combine the employee's first name and last name into a single field for reporting.
 SELECT CONCAT(firstName, lastName) as EmployeeName FROM employees
 WHERE reportsTo = (SELECT employeeNumber FROM employees WHERE firstName = 'Diane' AND lastName = 'Murphy');
@@ -238,13 +240,29 @@ SELECT productCode, productName, quantityInStock*100/total.sum AS percentageOfTo
 CROSS JOIN (SELECT SUM(quantityInStock) AS sum FROM products) total
 ORDER BY percentageOfTotal DESC
 -- 11. Write a function to convert miles per gallon to liters per 100 kilometers.
-create function 'litersPer100kilo'(input) returns INTEGER
-begin
-return 235.21/input;
-end;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `litersPer100kilo`(input decimal(10)) RETURNS decimal(10,0)
+    DETERMINISTIC
+BEGIN
+RETURN 235.21/input;
+END
+DELIMITER ;
+SELECT litersPer100kilo(27);
 
--- (not solved) 12. Write a procedure to increase the price of a specified product category by a given percentage. You will need to create a product table with appropriate data to test your procedure. Alternatively, load the ClassicModels database on your personal machine so you have complete access. You have to change the DELIMITER prior to creating the procedure.
+-- 12. Write a procedure to increase the price of a specified product category by a given percentage. You will need to create a product table with appropriate data to test your procedure. Alternatively, load the ClassicModels database on your personal machine so you have complete access. You have to change the DELIMITER prior to creating the procedure.
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `increasePrice`(
+IN product_Code VARCHAR(50), 
+IN percentageIncrease decimal(10))
+BEGIN
+	UPDATE products
+    SET MSRP = MSRP*(1+percentageIncrease/100)
+    WHERE product_Code = productCode;
+    
+END $$
+DELIMITER ;
 
+CALL increasePrice('S10_1949',10);
 
 -- 13. What is the value of orders shipped in August 2004? (Hint).
 SELECT SUM(quantityOrdered*priceEach) as OrderValue FROM orderdetails 
@@ -253,29 +271,83 @@ WHERE year(shippedDate) = 2004
 AND monthname(shippedDate) = 'August';
 
 -- (not solved) 14. What is the ratio the value of payments made to orders received for each month of 2004. (i.e., divide the value of payments made by the orders received)?
-SELECT orderNumber, SUM(quantityOrdered*priceEach) as OrderValue FROM orderdetails 
-GROUP BY orderNumber
-SELECT * FROM payments CROSS JOIN (SELECT * FROM orders)
-
+DROP VIEW paymentOrderValue;
+CREATE VIEW paymentOrderValue AS
+SELECT SUM(payments.amount) AS paymentValue, SUM(orderdetails.quantityOrdered*orderdetails.priceEach) as OrderValue, month(payments.paymentDate) AS eachMonth FROM orders
+JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber
+JOIN payments ON payments.customerNumber = orders.customerNumber
+WHERE year(payments.paymentDate)=2004
+GROUP BY month(payments.paymentDate)
+ORDER BY month(payments.paymentDate);
+SELECT OrderValue/paymentValue as ratio, eachMonth FROM paymentOrderValue;
 
 -- 15. What is the difference in the amount received for each month of 2004 compared to 2003?
-SELECT SUM(amount), month(paymentDate), paymentDate FROM payments
+DROP VIEW amount2004;
+CREATE VIEW amount2004 AS
+SELECT SUM(amount) AS amount4, month(paymentDate) AS month4 FROM payments
 WHERE year(paymentDate) = 2004
 GROUP BY month(paymentDate)
-ORDER BY month(paymentDate)
+ORDER BY month(paymentDate);
 
-SELECT SUM(amount), month(paymentDate), paymentDate FROM payments
+DROP VIEW amount2003;
+CREATE VIEW amount2003 AS
+SELECT SUM(amount) AS amount3, month(paymentDate) AS month3 FROM payments
 WHERE year(paymentDate) = 2003
 GROUP BY month(paymentDate)
-ORDER BY month(paymentDate)
+ORDER BY month(paymentDate);
 
-SELECT abs(months04-month03)
+SELECT amount2004.month4, amount3, amount4, (amount3-amount4) AS diff FROM amount2004
+JOIN amount2003 ON amount2003.month3 = amount2004.month4;
 
-Write a procedure to report the amount ordered in a specific month and year for customers containing a specified character string in their name.
-Write a procedure to change the credit limit of all customers in a specified country by a specified percentage.
-Basket of goods analysis: A common retail analytics task is to analyze each basket or order to learn what products are often purchased together. Report the names of products that appear in the same order ten or more times.
-ABC reporting: Compute the revenue generated by each customer based on their orders. Also, show each customer's revenue as a percentage of total revenue. Sort by customer name.
-Compute the profit generated by each customer based on their orders. Also, show each customer's profit as a percentage of total profit. Sort by profit descending.
+-- 16 Write a procedure to report the amount ordered in a specific month and year for customers containing a specified character string in their name.
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `orderAmountChecker`(IN months INT, IN years INT, IN nameChara VARCHAR(50))
+BEGIN
+SELECT customers.customerName, customers.customerNumber, SUM(orderdetails.quantityOrdered*orderdetails.priceEach) as OrderValue FROM orders
+JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber
+JOIN customers ON customers.customerNumber = orders.customerNumber
+WHERE month(orders.orderDate) = months
+AND year(orders.orderDate) = years
+AND customers.customerName LIKE CONCAT('%',nameChara,'%')
+GROUP BY customers.customerNumber;
+END
+DELIMITER ;
+CALL orderAmountChecker(2,2003,'imports');
+
+
+-- 17 Write a procedure to change the credit limit of all customers in a specified country by a specified percentage.
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `limitPercenChange`(IN countries VARCHAR(50), IN percentage decimal)
+BEGIN
+update customers
+SET creditLimit = creditLimit*(1+percentage/100)
+WHERE country = countries;
+END
+DELIMITER ;
+SET SQL_SAFE_UPDATES=0;
+CALL limitPercenChange('France',10);
+SELECT * from customers;
+SET SQL_SAFE_UPDATES=1;
+
+
+-- 18 (Not Solved)Basket of goods analysis: A common retail analytics task is to analyze each basket or order to learn what products are often purchased together. Report the names of products that appear in the same order ten or more times.
+select r1.orderNumber as orderNumber,r1.productCode as p1, r2.productCode as p2, count(*)
+from orderdetails as r1
+join 
+orderdetails as r2
+on r1.orderNumber = r2.orderNumber
+where r1.productCode !=r2.productCode
+group by  r1.productCode, r2.productCode
+having count(*) >=10
+
+-- 19 ABC reporting: Compute the revenue generated by each customer based on their orders. Also, show each customer's revenue as a percentage of total revenue. Sort by customer name.
+SELECT SUM(payments.amount) AS revenue, SUM(payments.amount)/(SELECT SUM(payments.amount) FROM payments) AS ratio,  payments.customerNumber AS customerNumbers, customers.customerName AS customerNames FROM payments
+JOIN customers ON payments.customerNumber = customers.customerNumber
+GROUP BY payments.customerNumber
+ORDER BY customerNames
+
+-- 20 Compute the profit generated by each customer based on their orders. Also, show each customer's profit as a percentage of total profit. Sort by profit descending.
+
 Compute the revenue generated by each sales representative based on the orders from the customers they serve.
 Compute the profit generated by each sales representative based on the orders from the customers they serve. Sort by profit generated descending.
 Compute the revenue generated by each product, sorted by product name.
